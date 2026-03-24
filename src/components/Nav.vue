131e9aef022bca0a818a241f8df416a0bc8e2876
dihyAmgh <d_megherfi@estin.dz>
2026-03-24 17:18:34 +0100

    ajout du vue-router et des views

---
+++ src/components/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="navbar">
+        <div class="navbar_logo">
+            <img src="../assets/logo_rec.svg">
+        </div>
+        <div class="navbar_links">
+            <router-link to="/">recommendation</router-link>
+            <router-link to="/profile">Profile</router-link>
+        </div>
+    </nav>
+</template>
+
+
