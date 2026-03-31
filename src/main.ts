80cfd478e9ec9be189d0220183aa28c044886b12
jesovel <liliajesover@gmail.com>
2026-03-31 14:32:51 +0200

    adding comments

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,17 @@ import outputs from "../amplify_outputs.json";
 import { get } from "aws-amplify/api";
 import { parseAmplifyConfig } from "aws-amplify/utils";
 
+// Configuration d'Amplify avec les informations extraites du fichier amplify_outputs.json
 const amplifyConfig = parseAmplifyConfig(outputs);
 
+// Extraction des informations de l'API REST à partir des sorties personnalisées
 const customOutputs = outputs as typeof outputs & {
   custom: {
     API: Record<string, { endpoint: string; region: string; apiName: string }>;
   };
 };
 
+// Configuration d'Amplify avec les informations extraites du fichier amplify_outputs.json
 Amplify.configure({
   ...amplifyConfig,
   API: {
@@ -24,14 +27,17 @@ Amplify.configure({
 
 createApp(App).mount("#app");
 
+// appel de l'API REST pour récupérer la date actuelle
 const restOperation = get({
   apiName: "Test_API",
   path: "date",
 });
 
+// Traitement de la réponse de l'API REST
 const { body } = await restOperation.response;
 const data = await body.json();
 
+// Vérification de la structure de la réponse et affichage de la date
 if (data && typeof data === "object" && "date" in data) {
   console.log(data.date);
 } else {
