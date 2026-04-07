ae1b4b853bd569e1243ffb0acc614af9abc93ea7
dihyAmgh <d_megherfi@estin.dz>
2026-04-07 09:47:31 +0200

    la navbar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,13 +1,15 @@
 <template>
     <nav class="navbar">
-        <div class="navbar_logo">
+        <div class="navbar_logo" >
             <img src="../assets/logo_rec.svg">
         </div>
         <div class="navbar_links">
-            <router-link to="/">recommendation</router-link>
-            <router-link to="/profile">Profile</router-link>
+            <div class="container_links">
+            <router-link to="/">Home</router-link>
+            <router-link to="/recommendations">Recommendations</router-link>
+            </div>
+            <router-link to="/profile"><font-awesome-icon icon="fa-solid fa-circle-user" size="lg" style="color: white;" /></router-link>
         </div>
     </nav>
 </template>
-
 
