e3887e6bf84f772881b77018770560334df00484
dihyAmgh <d_megherfi@estin.dz>
2026-04-07 23:01:43 +0200

    fonction delete account et sign out

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,3 +1,39 @@
+<script setup lang="ts">
+import { signOut } from 'aws-amplify/auth';
+import { deleteUser } from 'aws-amplify/auth';
+import selectedGenres from './FirstSigninPage.vue';
+
+async function signout(){
+await signOut()
+}
+
+
+async function handleDeleteUser() {
+  try {
+    await deleteUser();
+  } catch (error) {
+    console.log(error);
+  }
+}
+</script>
+
 <template>
-<h1>Profile</h1>
+  <div style="display: flex; flex-direction:row; justify-content:space-around; gap: 20px;padding-block: 2rem;">
+    <div style="border: 1px solid #ccc; padding: 10px;border-radius: 12px; width:55%; display:flex; flex-direction: column; align-items: center;">
+      <h1>Profile Page</h1>
+      <div v-for="genre in selectedGenres" :key="genre.genreId">
+        <p>{{ genre.name }}</p>
+      </div>
+      <FirstSigninPage/>
+    </div>
+
+    <div style="border: 1px solid #ccc;border-radius: 12px; padding: 10px; width:35%; display:flex; flex-direction:column;">
+     <button  @click="signout()">Sign Out</button>
+     <button @click="handleDeleteUser()">Delete account</button>
+  </div>
+</div>
 </template>
+
+<style scoped>
+
+</style>
