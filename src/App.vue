20459c2181d17cb7ec789b75b981c739ac6bbb96
dihyAmgh <d_megherfi@estin.dz>
2026-04-07 23:03:56 +0200

    App vue avec router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
 import { Authenticator } from '@aws-amplify/ui-vue';
 import "@aws-amplify/ui-vue/styles.css";
-import Nav from './components/Nav.vue'
+import { AuthenticateCognitoAction } from 'aws-cdk-lib/aws-elasticloadbalancingv2-actions';
+import Nav from '@/components/Nav.vue';
+import Footer from '@/components/Footer.vue';
+import FirstSigninPage from './views/FirstSigninPage.vue';
+
 </script>
 
 <template>
+ <Nav/>
   <main>
     <authenticator>
       <template v-slot="{user, signOut}">
-        <router-view/>
-        <!--<Nav/>-->
-        <!--<button  @click="signOut">Sign Out </button>-->
+        <RouterView/>
+        <!--<RouterView v-if="hasCompleted"/>
+        <FirstSigninPage v-else-if="!hasCompleted"/>-->
       </template>
     </authenticator>
   </main>
+  <Footer/>
 </template>
