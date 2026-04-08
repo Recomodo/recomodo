84731e95ac8e9f336e8b9ad96f0f5b5660b8bf19
jesovel <liliajesover@gmail.com>
2026-04-08 22:38:12 +0200

    refactor: remove Todos component and fix loginId typo in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
 import { Authenticator } from '@aws-amplify/ui-vue';
 import "@aws-amplify/ui-vue/styles.css";
-import Todos from './components/Todos.vue'
-import { AuthenticateCognitoAction } from 'aws-cdk-lib/aws-elasticloadbalancingv2-actions';
+//import Todos from './components/Todos.vue'
+//import { AuthenticateCognitoAction } from 'aws-cdk-lib/aws-elasticloadbalancingv2-actions';
 </script>
 
 <template>
   <main>
     <authenticator>
       <template v-slot="{user, signOut}">
-        <h1>Hello {{ user?.signInDetails?.liginId }}'s todo</h1>
-        <Todos />
+        <h1>Hello {{ user?.signInDetails?.loginId }}</h1>
         <button  @click="signOut">Sign Out </button>
       </template>
     </authenticator>
