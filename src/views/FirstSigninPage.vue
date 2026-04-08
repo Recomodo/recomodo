cff175c36572ad7e4b2c54ac7c662801ec0c4002
dihyAmgh <d_megherfi@estin.dz>
2026-04-08 23:08:56 +0200

    ajout de l'api et fontawsome

--- src/views/FirstSigninPage.vue
+++ src/views/FirstSigninPage.vue
@@ -1,37 +1,31 @@
 <script setup lang="ts">
 //récuperer le liste des genres dans un tableau
-import { ref } from 'vue';
-const genresSections=[      // à remplacer par l'API
-    {
-        genreId: "1",
-        name: "Action",
-    },
-    {
-        genreId: "2",
-        name: "Comedy"
-    },
-    {
-        genreId: "3",
-        name: "Drama"
-    },
-    {
-        genreId: "4",
-        name: "Horror"
-    },
-    {
-        genreId: "5",
-        name: "Science Fiction"
-    },
-    {
-        genreId: "6",
-        name: "Romance"
-    }
-];
+import { ref, onMounted } from 'vue';
+
+//api
+import type {Schema} from "../../amplify/data/resource";
+import { generateClient } from 'aws-amplify/data';
+const client = generateClient<Schema>();
+const genresSections = ref<Array<Schema['Genre']["type"]>>([]);
+
+console.log("MODELS:", client.models);
+const res = await client.models.Genre.list();
+
+console.log("RAW RESPONSE:", res);
+console.log("DATA:", res.data);
+console.log("ERRORS:", res.errors);
+onMounted(async()=>{
+   const{data}=await client.models.Genre.list({
+      limit: 20,
+   });
+   console.log("GENRES:", data);
+   genresSections.value=data;
+})
 
 const maxSelection = 3;
-const selectedGenres = ref<string[]>([]);
+const selectedGenres = ref<number[]>([]);
 
-function clickedGenre(genreId: string) {
+function clickedGenre(genreId: number) {
    if (selectedGenres.value.includes(genreId)) {
       selectedGenres.value = selectedGenres.value.filter(id => id !== genreId);
    } else if (selectedGenres.value.length < maxSelection) {
@@ -39,11 +33,11 @@ function clickedGenre(genreId: string) {
    }
 }
 
-function isSelected(genreId: string) {
+function isSelected(genreId: number) {
    return selectedGenres.value.includes(genreId);
 }
 
-function isDisabled(genreId: string) {
+function isDisabled(genreId: number) {
    return !selectedGenres.value.includes(genreId) && selectedGenres.value.length >= maxSelection;
 }
 
@@ -56,8 +50,8 @@ function isDisabled(genreId: string) {
    <h1>Première connexion</h1>
    <div>
       <p>veuillez sélectionner jusqu'à trois genres de films qui vous passionnent le plus. N'hésitez pas à explorer différentes catégories pour découvrir de nouveaux films et élargir vos horizons cinématographiques. Votre sélection nous permettra de vous offrir une expérience personnalisée et de vous recommander des films qui correspondent parfaitement à vos préférences.</p>
-         <button class="genreSection" :class="{ selected: isSelected(genre.genreId), disabled: isDisabled(genre.genreId) }" v-for="genre in genresSections" :key="genre.genreId"  @click="clickedGenre(genre.genreId) ">
-            <label :for="genre.genreId">{{ genre.name }} </label>
+         <button class="genreSection" :class="{ selected: isSelected(genre.genreId), disabled: isDisabled(genre.genreId) }" v-for="genre in genresSections" :key="genre.id"  @click="clickedGenre(genre.genreId) ">
+            <label :for="String(genre.genreId)">{{ genre.name }} </label>
          </button>
    </div>
 
