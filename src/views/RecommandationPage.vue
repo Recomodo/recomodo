54847a083809e28109a56355e06680f29bac4054
dihyAmgh <d_megherfi@estin.dz>
2026-04-17 17:02:19 +0200

    changer la redirection

--- src/views/RecommandationPage.vue
+++ src/views/RecommandationPage.vue
@@ -5,55 +5,27 @@ import { onMounted, ref } from 'vue';
 import { generateClient } from 'aws-amplify/data';
 const router = useRouter();
 
-const listMoviesQuery = `
-  query ListMovies {
-    listMovies(limit: 20) {
-      items {
-        movieId
-        title
-        voteAverage
-        posterPath
-      }
-    }
-  }
-`;
-
 
 const client = generateClient<Schema>();
-
-
 const movies = ref<Array<Schema['Movie']["type"]>>([]);
+
+
 onMounted(async () => {
-    try{
-  const res = await client.graphql({
-    query: listMoviesQuery
+    try {
+      const { data, errors } = await client.models.Movie.list({
+        limit: 20
+      });
+      movies.value=data ?? [];
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
   });
-  console.log("FULL RES:", res);
-  const items = res.data?.listMovies?.items;
-  movies.value = Array.isArray(items) ? items : [];
-}catch(error){
-  console.error("Error fetching movies:", error);
-}
-});
-
-/*const movies = ref<Array<Schema['Movie']["type"]>>([]);
-
-function listMovies() {
-  client.models.Movie.observeQuery().subscribe({
-    next: ({ items, isSynced }) => {
-      movies.value = items
-     },
-  }); 
-}
-
-onMounted(
-  listMovies()
-)*/
-
 
 function filmDetails(movieId:string){
-    router.push(`/movie/details/id:${movieId}`);
+    router.push(`/movie/details/${movieId}`);
 }
+
+
 </script>
 
 
