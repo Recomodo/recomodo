27f8c78c39e347e31bf79529b6a10ab7464b2517
dihyAmgh <d_megherfi@estin.dz>
2026-04-19 17:48:33 +0200

    router avec homeview et non homepage

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory} from "vue-router"
-import HomePage from "../views/HomePage.vue"
+import HomeView from "@/views/HomeView.vue"
 
 const routes = [
     {
@@ -21,7 +21,7 @@ const routes = [
     {
         path:"/",
         name:"home",
-        component: HomePage,
+        component: HomeView,
     }
 ]
 
