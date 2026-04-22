7bf86046d8d089199ea0655d2fdd511adea71fce
jesovel <liliajesover@gmail.com>
2026-04-23 00:01:39 +0200

    uncomment functionName in similar function definition

--- amplify/functions/similar/resource.ts
+++ amplify/functions/similar/resource.ts
@@ -11,7 +11,7 @@ const functionDir = path.dirname(fileURLToPath(import.meta.url));
  
 export const similar = defineFunction((scope) => {
   const lambda = new Function(scope, "similar", {
-    //functionName: "similar",
+    functionName: "similar",
     runtime: Runtime.PYTHON_3_10,
     handler: "similar.handler",
     timeout: Duration.seconds(10), // plus court que le recommender car c'est juste un .get() sur le JSON
