163cce83275b9b11ecb6959a5d1d683c5771e7f1
dihyAmgh <d_megherfi@estin.dz>
2026-04-29 12:14:47 +0200

    suppression de on input

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,6 +1,6 @@
 <template>
 
-<input type="text" placeholder="Search " id="titre_film" class="searchBar" v-model="titre" @input="onInput"> <!--v-model => titre.value="ce que l'utilisateur entre" -->
+<input type="search" placeholder="Search...  🔍" id="titre_film" class="searchBar" v-model="titre"  @keydown.enter="emitSearch" > <!--v-model => titre.value="ce que l'utilisateur entre" --> <!--@input="onInput"-->
 
 </template>
 
@@ -13,25 +13,28 @@ const emit = defineEmits<{
     (e:"search", value:string): void;
 }>();
 
-/*function emitSearch(){
+function emitSearch(){
+    clearTimeout(timeout);
     emit("search", titre.value)
-}*/
+}
 let timeout: ReturnType<typeof setTimeout>;
 
 function onInput() {
     clearTimeout(timeout);
-
     timeout = setTimeout(() => {
         emit("search", titre.value);
-    }, 300);
+    }, 400);
 }
 
 </script>
-
 <style>
 .searchBar{
-    width: 20%;
-    display: block;
-    padding: 0.3rem;
+    border-radius: 7px;
+    padding-inline: 12px;
+    padding-block: 3px;
 }
+.searchBar:hover{
+    border-color: rgb(239, 162, 239);
+}
+
 </style>
