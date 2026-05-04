aec9b5f0e9aa7d9a92b1d3978816c4e35e62db82
Dinaa18 <bouderkadina@gmail.com>
2026-05-04 17:51:31 +0200

    essai defaultposter v3

--- src/utils/defaultPoster.ts
+++ src/utils/defaultPoster.ts
@@ -1,8 +1,10 @@
 import posterDEF from '@/assets/posterDEF.jpg'
+
 export function handleImageError(event: Event) {
   const target = event.target as HTMLImageElement | null;
-  if (target) {
-    target.src = posterDEF;
+
+  if (!target) {
+    return;
   }
 
   if (target.dataset.fallbackbackup === 'true') {
