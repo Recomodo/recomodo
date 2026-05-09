700a51d175b7c08f5d1ca1f186cef3be5264414c
jesovel <liliajesover@gmail.com>
2026-05-09 19:59:43 +0200

    fin des tests unitaires de similar

--- tests/test_similar.py
+++ tests/test_similar.py
@@ -41,6 +41,41 @@ def test_extract_movie_id_missing():
 
 #Teste que la fonction handler lève une erreur si userId est absent de l'événement
 def test_handler_raises_when_user_id_missing():
-    with pytest.raises(ValueError, match="userId not found in event"):
+    with pytest.raises(ValueError, match="movieId not found in event"):
         similar.handler({}, None)
 
+#Teste que la fonction handler retourne une liste vide de films similaires si le movieId n'est pas trouvé dans les recommandations pré-calculées
+def test_handler_returns_empty_when_no_similar(monkeypatch):
+    event = {"movieId": "1"} 
+    
+    fake_similar = {
+        "10": ["20", "21", "22"],
+        "11": ["21", "23", "24"]
+    }
+
+    #Simule les fonctions de chargement des données pour retourner les données factices
+    monkeypatch.setattr(similar, "load_recommendations_from_s3", lambda: fake_similar)
+
+    result = similar.handler(event, None)
+
+    assert result == {
+        "movieId": "1",
+        "similar": []
+    }
+
+def test_handler_returns_similar(monkeypatch):
+    event = {"movieId": "10"} 
+    
+    fake_similar = {
+        "10": ["20", "21", "22"],
+        "11": ["21", "23", "24"]
+    }
+
+    monkeypatch.setattr(similar, "load_recommendations_from_s3", lambda: fake_similar)
+
+    result = similar.handler(event, None)
+
+    assert result == {
+        "movieId": "10",
+        "similar": ["20", "21", "22"]
+    }
